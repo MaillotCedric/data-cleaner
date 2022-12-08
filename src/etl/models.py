e48245ccc505237d08cf30b8257f6e222fe3ba8b
Cédric MAILLOT <cedricmaillot38@gmail.com>
2022-12-08 11:07:45 +0400

    Fix : nom des colonnes du modèle
    . Suppression des majuscules

--- src/etl/models.py
+++ src/etl/models.py
@@ -1,26 +1,26 @@
 from django.db import models
 
 class Pays(models.Model):
-    Country = models.CharField(primary_key=True, max_length=100)
+    country = models.CharField(primary_key=True, max_length=100)
     class Meta:
         db_table = 'pays'
 
 class Commande(models.Model):
-    InvoiceNo = models.CharField(primary_key=True, max_length=100)
-    Country = models.ForeignKey(Pays, on_delete=models.CASCADE)
+    invoice_no = models.CharField(primary_key=True, max_length=100)
+    country = models.ForeignKey(Pays, on_delete=models.CASCADE)
     class Meta:
         db_table = 'commande'
 
 class Produit(models.Model):
-    StockCode = models.CharField(primary_key=True, max_length=100)
+    stock_code = models.CharField(primary_key=True, max_length=100)
     class Meta:
         db_table = 'produit'
 
 class Details_commande(models.Model):
     id_details_commande = models.BigAutoField(primary_key=True)
-    InvoiceDate = models.DateField()
-    InvoiceNo = models.ForeignKey(Commande, on_delete=models.CASCADE)
-    StockCode = models.ForeignKey(Produit, on_delete=models.CASCADE)
+    invoice_date = models.DateField()
+    invoice_no = models.ForeignKey(Commande, on_delete=models.CASCADE)
+    stock_code = models.ForeignKey(Produit, on_delete=models.CASCADE)
     class Meta:
         db_table = 'details_commande'
-        unique_together = [['InvoiceNo', 'StockCode']]
+        unique_together = [['invoice_no', 'stock_code']]
