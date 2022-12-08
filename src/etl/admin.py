fc5d1a80eba5e6a7bc99d1621cb8cd23a30b075f
Cédric MAILLOT <cedricmaillot38@gmail.com>
2022-12-08 10:23:47 +0400

    Mise en place du modèle
    . Création des tables
    . Enregistrement des tables sur l'interface administrateur

--- src/etl/admin.py
+++ src/etl/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from.models import Pays, Commande, Produit, Details_commande
 
-# Register your models here.
+admin.site.register(Pays)
+admin.site.register(Commande)
+admin.site.register(Produit)
+admin.site.register(Details_commande)
--- src/etl/models.py
+++ src/etl/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Pays(models.Model):
+    Country = models.CharField(primary_key=True, max_length=100)
+    class Meta:
+        db_table = 'pays'
+
+class Commande(models.Model):
+    InvoiceNo = models.CharField(primary_key=True, max_length=100)
+    Country = models.ForeignKey(Pays, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'commande'
+
+class Produit(models.Model):
+    StockCode = models.CharField(primary_key=True, max_length=100)
+    class Meta:
+        db_table = 'produit'
+
+class Details_commande(models.Model):
+    id_details_commande = models.BigAutoField(primary_key=True)
+    InvoiceDate = models.DateField()
+    InvoiceNo = models.ForeignKey(Commande, on_delete=models.CASCADE)
+    StockCode = models.ForeignKey(Produit, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'details_commande'
+        unique_together = [['InvoiceNo', 'StockCode']]
