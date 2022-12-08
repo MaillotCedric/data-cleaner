8e7f9fb606f5f3ef0abbec68e35b198027d750c3
Cédric MAILLOT <cedricmaillot38@gmail.com>
2022-12-08 13:43:27 +0400

    Fix : lisibilité du code

--- src/etl/admin.py
+++ src/etl/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from.models import Pays, Commande, Produit, Details_commande
+from .models import Pays, Commande, Produit, Details_commande
 
 admin.site.register(Pays)
 admin.site.register(Commande)
