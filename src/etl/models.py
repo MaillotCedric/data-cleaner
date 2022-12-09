b198f01ceb5dbe2f4d32164d280247848ed06df2
Cédric MAILLOT <cedricmaillot38@gmail.com>
2022-12-09 10:09:58 +0400

    Fix : optimisation du modèle
    . L'utilisation de la méthode raw() implique d'avoir des id auto incrémentés en PK

--- src/etl/models.py
+++ src/etl/models.py
@@ -1,23 +1,31 @@
 from django.db import models
 
 class Pays(models.Model):
-    country = models.CharField(primary_key=True, max_length=100)
+    country = models.CharField(max_length=100)
     class Meta:
         db_table = 'pays'
+        constraints = [
+            models.UniqueConstraint(fields=["country"], name="country unique")
+        ]
 
 class Commande(models.Model):
-    invoice_no = models.CharField(primary_key=True, max_length=100)
+    invoice_no = models.CharField(max_length=100)
     country = models.ForeignKey(Pays, on_delete=models.CASCADE)
     class Meta:
         db_table = 'commande'
+        constraints = [
+            models.UniqueConstraint(fields=["invoice_no"], name="invoice_no unique")
+        ]
 
 class Produit(models.Model):
-    stock_code = models.CharField(primary_key=True, max_length=100)
+    stock_code = models.CharField(max_length=100)
     class Meta:
         db_table = 'produit'
+        constraints = [
+            models.UniqueConstraint(fields=["stock_code"], name="stock_code unique")
+        ]
 
 class Details_commande(models.Model):
-    id_details_commande = models.BigAutoField(primary_key=True)
     invoice_date = models.DateField()
     invoice_no = models.ForeignKey(Commande, on_delete=models.CASCADE)
     stock_code = models.ForeignKey(Produit, on_delete=models.CASCADE)
