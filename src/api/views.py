c3465de2c897306008fac511c85aa647e2d38f8a
MaillotCedric <cedricmaillot38@gmail.com>
2022-12-11 11:44:49 +0400

    Refactor : import
    . import des serializers

--- src/api/views.py
+++ src/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.contrib.auth.models import User
-from .serializers import UsersSerializer, SalesByProductsSerializer, SalesByCountriesSerializer, SalesOfSerializer
+from .serializers import *
 
 @api_view(["GET"])
 def get_utilisateurs(request):
