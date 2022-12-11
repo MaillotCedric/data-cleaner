b55016b20ab3742a9b602f40414d9cbb1e393b51
MaillotCedric <cedricmaillot38@gmail.com>
2022-12-11 18:09:51 +0400

    Add : bouton requête
    . répartition des ventes par pays ( Top 3 )

--- src/app/static/js/chart.js
+++ src/app/static/js/chart.js
@@ -1,1 +1,2 @@
 ajouter_ajax_call("#btn_ventes_produits", "GET", "../api/sales_by_products?top=3&format=json");
+ajouter_ajax_call("#btn_ventes_pays", "GET", "../api/sales_by_countries?top=3&format=json");
