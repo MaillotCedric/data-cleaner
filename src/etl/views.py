61a57dec3ba0d62071af91db82b6bb533d82b02e
Cédric MAILLOT <cedricmaillot38@gmail.com>
2022-12-13 13:20:32 +0400

    Mise en place de l'importation de fichiers

--- src/etl/views.py
+++ src/etl/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from io import TextIOWrapper
+from .forms import UploadFileForm
+
+def render_etl(request):
+    form = UploadFileForm()
+
+    return render(request, "etl.html", {"form": form})
 
 @login_required(login_url="login")
 def index(request):
-    return render(request, "etl.html", {})
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            # encodage du fichier
+            file = TextIOWrapper(request.FILES['fichier'], encoding="iso-8859-1", newline="")
+
+            return render(request, "etl.html", {"form": form})
+        else:
+            return render_etl(request)
+    else:
+        return render_etl(request)
