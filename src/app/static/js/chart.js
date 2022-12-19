f2b77dbbc8d0d2be283d621d4cbf11005f7e2f36
MaillotCedric <cedricmaillot38@gmail.com>
2022-12-19 13:54:13 +0400

    Fix : graphiques
    . désactivation des appels ajax obsolètes
    . Chart.js : changement de CDN

--- src/app/static/js/chart.js
+++ src/app/static/js/chart.js
@@ -49,31 +49,31 @@ function update_chart_produits(json) {
 ajax_call("GET", "../api/sales_by_products?top=10&format=json", donnees={}, success_callback=update_chart_produits, error_callback=afficher_error);
 
 // Populate select with countries names
-$.ajax({
-    type: "GET",
-    url: "../api/countries?format=json",
-    success: function(json) {
-        let select = document.getElementById("select_pays");
+// $.ajax({
+//     type: "GET",
+//     url: "../api/countries?format=json",
+//     success: function(json) {
+//         let select = document.getElementById("select_pays");
 
-        json.forEach(element => {
-            let pays = element.country;
+//         json.forEach(element => {
+//             let pays = element.country;
 
-            select.innerHTML = select.innerHTML + `<option value="`+ pays +`">`+ pays +`</option>`;
-        });
-    },
-    error: function(erreur) {
-        afficher_error(erreur)
-    }
-});
+//             select.innerHTML = select.innerHTML + `<option value="`+ pays +`">`+ pays +`</option>`;
+//         });
+//     },
+//     error: function(erreur) {
+//         afficher_error(erreur)
+//     }
+// });
 
-ajouter_ajax_call("#btn_ventes_produits", "GET", "../api/sales_by_products?top=3&format=json");
-ajouter_ajax_call("#btn_ventes_pays", "GET", "../api/sales_by_countries?top=3&format=json");
+// ajouter_ajax_call("#btn_ventes_produits", "GET", "../api/sales_by_products?top=3&format=json");
+// ajouter_ajax_call("#btn_ventes_pays", "GET", "../api/sales_by_countries?top=3&format=json");
 
 // add ajax call on form ventes_pays_produits
-$(document).on('submit', "#btn_ventes_pays_produits", function(event){
-    let select = document.getElementById("select_pays");
-    let pays = select.value;
+// $(document).on('submit', "#btn_ventes_pays_produits", function(event){
+//     let select = document.getElementById("select_pays");
+//     let pays = select.value;
 
-    event.preventDefault();
-    ajax_call("GET", "../api/sales_of?pays="+ pays +"&top=3&format=json", donnees={}, success_callback=afficher_success, error_callback=afficher_error);
-});
+//     event.preventDefault();
+//     ajax_call("GET", "../api/sales_of?pays="+ pays +"&top=3&format=json", donnees={}, success_callback=afficher_success, error_callback=afficher_error);
+// });
