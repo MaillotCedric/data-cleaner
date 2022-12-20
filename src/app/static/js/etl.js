3d602989fcb3e0d1f3bcaed6036e45ca9b76ce12
MaillotCedric <cedricmaillot38@gmail.com>
2022-12-20 10:51:30 +0400

    UI : import
    . UI : import fichier
    . ajout modal fade
    . UI : message d'attente

--- src/app/static/js/etl.js
+++ src/app/static/js/etl.js
@@ -1,17 +1,35 @@
 btn_import = document.getElementById("btn_import_csv");
 btn_import_bdd = document.getElementById("btn_import_bdd");
 div_loading = document.getElementById("loading_message");
+file_input = document.getElementById("id_fichier");
+loading_modal = document.getElementById("loading_modal");
 
 function est_present(element_html) {
     return element_html != null;
 };
 
+function est_visible(btn) {
+    return !btn.classList.value.split(" ").includes("d-none");
+};
+
+function enlever_classe(nom_classe, element_html) {
+    element_html.classList.remove(nom_classe);
+};
+
 btn_import.addEventListener("click", () => {
-    div_loading.innerHTML = "Nettoyage en cours ..."
+    enlever_classe("d-none", loading_modal);
+    div_loading.innerHTML = "Nettoyage en cours ...";
 });
 
 if (est_present(btn_import_bdd)) {
     btn_import_bdd.addEventListener("click", () => {
-        div_loading.innerHTML = "Importation en bdd ..."
+        enlever_classe("d-none", loading_modal);
+        div_loading.innerHTML = "Importation en base de données ...";
     });
 };
+
+file_input.addEventListener("input", () => {
+    if (!est_visible(btn_import)) {
+        enlever_classe("d-none", btn_import);
+    }
+});
